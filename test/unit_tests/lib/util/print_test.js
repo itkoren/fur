02fd14d7d4187a49b293348aa525574584c39b95
Taka Okunishi <okunishinishi@gmail.com>
2014-05-03 23:21:43 +0900

    add test

--- test/unit_tests/lib/util/print_test.js
+++ test/unit_tests/lib/util/print_test.js
@@ -7,15 +7,37 @@
 var print = require('../../../../lib/util/print.js'),
     testHelper = require('../../../../test/test_helper');
 
-exports.setUp = function(done) {
+exports.setUp = function (done) {
+    console.____log = console.log;
+    console.log = function () {
+    };
     done();
 };
 
-exports.tearDown = function(done) {
+exports.tearDown = function (done) {
+    console.log = console.____log;
+    delete console.____log;
     done();
 };
 
 exports['Print.'] = function (test) {
-    test.ifError(new Error('Not implemented!'));
+    ['FALSE', 'TRUE'].forEach(function (flg) {
+        process.env.FUR_PRINT_DISABLED = flg;
+        print('Normal print.', 1);
+        print(new Error(), {foo: 'bar'});
+        print.info('Info print.', 2);
+        print.ng('NG print.', 3);
+        print.ng.detail('NG Detail print.', 4);
+        print.ok('OK print.', 5);
+        print.debug('foo');
+        print._indents.push(10);
+        print.debug('bar');
+        print._indents.pop();
+    });
+    process.env.APEMAN_PRINT_DISABLED = 'FALSE';
+    print('foo', 'bar');
+
+
+    print._log('foo', 'bar');
     test.done();
 };
